b8658fc1ad682cf6589088d7e245a3531999187b
terrykingcha <terrykingcha@gmail.com>
2016-12-23 02:24:56 +0800

    update

--- src/cloud.js
+++ src/cloud.js
@@ -27,7 +27,7 @@ export default class Cloud extends CanvasImage {
     drawImages(hovers, focus, scrollX, scrollY) {
         const params = [];
         const ids = [];
-        const scale = 0.4;
+        const scale = 0.15;
 
         const pushParams = id => {
             if (ids.indexOf(id) < 0
@@ -88,25 +88,48 @@ export default class Cloud extends CanvasImage {
 
         if (slice) {
             let {
-                img,
+                imgs,
                 render
             } = slice;
 
             if (!cleared) {
                 const duration = 1000;
+                const scale = 3;
 
                 return ({
                     delta,
                     elapsed
                 }) => {
-                    if (elapsed <= duration) {
-                        render.globalAlpha -= delta / duration;
+                    if (elapsed <= duration &&
+                            render.globalAlpha > 0) {
+                        const ratio = delta / duration;
+
+                        slice.offsetLeft -= ratio * this.sliceWidth / scale;
+                        slice.offsetRight += ratio * this.sliceWidth / scale;
+                        slice.offsetTop -=  ratio * this.sliceHeight / scale;
+                        slice.offsetBottom +=  ratio * this.sliceHeight / scale;
+                        render.globalAlpha -= ratio;
                     } else {
+                        slice.offsetLeft = - this.sliceWidth / scale;
+                        slice.offsetRight = this.sliceWidth / scale;
+                        slice.offsetTop = -this.sliceHeight / scale;
+                        slice.offsetBottom = this.sliceHeight / scale;
                         render.globalAlpha = 0;
                         focus.cleared = true;
                     }
+                    
                     render.clearRect(0, 0, this.sliceWidth, this.sliceHeight);
-                    render.drawImage(img, 0, 0, this.sliceWidth, this.sliceHeight);
+
+                    render.save();
+                    render.globalAlpha /= 2;
+                    render.drawImage(imgs.center, 0, 0, this.sliceWidth, this.sliceHeight);
+                    render.restore();
+
+                    render.drawImage(imgs.left, slice.offsetLeft, 0, this.sliceWidth, this.sliceHeight);
+                    render.drawImage(imgs.top, 0, slice.offsetTop, this.sliceWidth, this.sliceHeight);
+                    render.drawImage(imgs.right, slice.offsetRight, 0, this.sliceWidth, this.sliceHeight);
+                    render.drawImage(imgs.bottom, 0, slice.offsetBottom, this.sliceWidth, this.sliceHeight);
+
                     return focus.cleared;
                 }
             }
@@ -114,19 +137,30 @@ export default class Cloud extends CanvasImage {
     }
 
     ready() {
-        this.slices = {};
+        const imgs = {
+            all: this.items['cloud'].obj,
+            center: this.items['cloud1'].obj,
+            left: this.items['cloud2'].obj,
+            top: this.items['cloud3'].obj,
+            right: this.items['cloud4'].obj,
+            bottom: this.items['cloud5'].obj,
+        };
 
-        const img = this.items['cloud'].obj;
+        this.slices = {};
 
         for (let i = 1; i <= this.hSlice * this.vSlice; i++) {
             const x = (i - 1) % this.hSlice;
             const y = parseInt((i - 1) / this.hSlice);
-            const [canvas, render] = img2Canvas(img, this.sliceWidth, this.sliceHeight);
+            const [canvas, render] = img2Canvas(imgs.all, this.sliceWidth, this.sliceHeight);
 
             this.slices[String(i - 1)] = {
-                img,
+                imgs,
                 canvas,
                 render,
+                offsetLeft: 0,
+                offsetTop: 0,
+                offsetRight: 0,
+                offsetBottom: 0,
                 x: x * this.sliceWidth,
                 y: y * this.sliceHeight,
                 width: this.sliceWidth,
